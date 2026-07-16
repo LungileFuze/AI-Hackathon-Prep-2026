ac8ec010da9c396589e7906ee5deb2ec741a31cd
LungileFuze <ngcobolungile94@outlook.com>
2026-07-16 15:21:04 +0200

    Added folders and database configurations

---
+++ Backend/Hackathon.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Hackathon.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hackathon.API.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes => Set<Note>();
+    }
+}
---
+++ Backend/Hackathon.API/Models/Entities/Note.cs
@@ -0,0 +1,15 @@
+﻿namespace Hackathon.API.Models.Entities
+{
+    public class Note
+    {
+        public  int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;   
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+
+    }
+}
